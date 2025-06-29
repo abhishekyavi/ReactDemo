b280c0eaccc4cc0ed2efbf1c87ed8ea624c3e9a6
abhishekyavi <158139103+abhishekyavi@users.noreply.github.com>
2025-06-29 06:16:18 +0000

    added Test component

--- clinicals-app/src/App.js
+++ clinicals-app/src/App.js
@@ -5,6 +5,7 @@ import AddPatient from './components/AddPatient';
 import AddClinicals from './components/AddClinicals';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Test from './components/Test';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Test />} />
           <Route path="/AddPatient" element={<AddPatient />} />
           <Route path="/AddPatient/:id" element={<AddPatient />} />
           <Route path="/AddClinicals/:id" element={<AddClinicals />} />
---
+++ clinicals-app/src/components/Test.js
@@ -0,0 +1,12 @@
+
+function Test() {
+  return (
+    <div>
+      
+      <h2>Test Component</h2>
+      <p>This is a placeholder for the Test component.</p>
+      <p>You can add your test logic here.</p>
+    </div>
+  );
+}
+export default Test;
